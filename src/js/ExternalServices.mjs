eabfd00fc174e865bdca555b753c4d1ad424598d
BelChaparro <a.belenchaparro@gmail.com>
2023-03-13 21:38:09 -0300

    added getPokeTypes method

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -10,15 +10,24 @@ function convertToJson(res) {
 
 export default class ExternalServices {
 
-  async getPokemonsByGeneration(genNumber) {
-    const response = await fetch(baseURL + `generation/${genNumber}/`);
-    const data = await convertToJson(response);
-    return data.Result;
-  }
-  async getPokemonsByType(type) {
-    const products = await fetch(baseURL + `type/${type}/`);
-    const data = await convertToJson(products);
-    return data.Result;
-  }
+    // Get JSON of types (count,results->name[0]->url[1]):
+    async getPokeTypes() {
+        const response = await fetch(baseURL + "type/");
+        const data = await convertToJson(response);
+        return data.Result;
+    }
+
+    // Gets JSON (with pokemon_species->[6] and types->[7]) from given URL:
+    async getPokemonsByGeneration(genNumber) {
+        const response = await fetch(baseURL + `generation/${genNumber}/`);
+        const data = await convertToJson(response);
+        return data.Result;
+    }
+    // Gets JSON (with name->[5] and pokemon->[9]) from given URL:
+    async getPokemonsByType(type) {
+        const products = await fetch(baseURL + `type/${type}/`);
+        const data = await convertToJson(products);
+        return data.Result;
+    }
 
 }
