d77940811b14254e854b3a3df0a1a53875d3db39
BelChaparro <a.belenchaparro@gmail.com>
2023-03-16 00:59:58 -0300

    corrected the data return

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -14,27 +14,27 @@ export default class ExternalServices {
     async getPokeTypes() {
         const response = await fetch(baseURL + "type/");
         const data = await convertToJson(response);
-        return data.Result;
+        return data.results;
     }
 
     // Get JSON of generations (count, results->name[0]->url[1]):
     async getPokeGenerations() {
       const response = await fetch(baseURL + "generation/");
       const data = await convertToJson(response);
-      return data.Result;
+      return data.results;
   }
 
     // Gets JSON (with pokemon_species->[6] and types->[7]) from given URL:
     async getPokemonsByGeneration(genNumber) {
         const response = await fetch(baseURL + `generation/${genNumber}/`);
         const data = await convertToJson(response);
-        return data.Result;
+        return data.results;
     }
     // Gets JSON (with name->[5] and pokemon->[9]) from given URL:
     async getPokemonsByType(type) {
         const products = await fetch(baseURL + `type/${type}/`);
         const data = await convertToJson(products);
-        return data.Result;
+        return data.results;
     }
 
 }
