bea6fe4c16137ead1178cd7c4b529092a242627f
BelChaparro <a.belenchaparro@gmail.com>
2023-03-16 01:30:02 -0300

    removed options parent elements

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -2,9 +2,7 @@ import ExternalServices from "./ExternalServices.mjs";
 import HomePage from "./HomePage.mjs";
 
 const dataSource = new ExternalServices();
-const genOptionsElement = document.querySelector("#generations");
-const typeOptionsElement = document.querySelector("type-form");
-const parentElement = document.querySelector(".main-content");
-const homePage = new HomePage(dataSource, genOptionsElement, typeOptionsElement, parentElement);
+const mainContainer = document.querySelector(".main-content");
+const homePage = new HomePage(dataSource, mainContainer);
 
 homePage.init();
