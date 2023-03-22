2b4eced612a999f5ca58ae71d9bf53309abfecbb
BelChaparro <a.belenchaparro@gmail.com>
2023-03-21 21:33:28 -0300

    added id and name attributes to forms

--- src/js/HomePage.mjs
+++ src/js/HomePage.mjs
@@ -4,15 +4,15 @@ function homePageTemplate() {
     return `<h1>Welcome to the Pokémon Generator!</h1>
 
             <h2>Select Pokémons based on generation</h2>
-            <form id="generation-form">
+            <form id="generation-form" name="gen-form">
                 <label for="generations">Generation:</label>
                 <select name="generations" id="generations" required></select>
-                <button type="submit">Show Pokemons!</button>
+                <button id="gen-btn" type="submit">Show Pokemons!</button>
             </form>
 
             <h2>Select Pokémons based on type</h2>
-            <form id="type-form">
-                <button type="submit">Show Pokemons!</button>
+            <form id="type-form" name="type-form">
+                <button id="type-btn" type="submit">Show Pokemons!</button>
             </form>`;
 }
 
@@ -34,6 +34,10 @@ export default class HomePage {
     async init () {
         // Fill the title with the name of the page:
         document.querySelector(".page-title").textContent = "Home Page | PokéGen";
+
+        // Add footer year:
+        document.querySelector("#footer-year").textContent =  new Date().getFullYear();
+        
         // Await promise from dataSource:
         const generationsList = await this.dataSource.getPokeGenerations();
         const typesList = await this.dataSource.getPokeTypes();
