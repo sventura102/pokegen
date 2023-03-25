6d184d62dbbe5144e3f25b42b0104f70a209a370
BelChaparro <a.belenchaparro@gmail.com>
2023-03-24 23:50:40 -0300

    called rendering functions

--- src/js/PokeList.mjs
+++ src/js/PokeList.mjs
@@ -1,12 +1,12 @@
-import { renderListWithTemplate } from "./utils.mjs";
+import { renderWithTemplate, renderListWithTemplate, getLocalStorage } from "./utils.mjs";
 
-function pokeListMainTemplate(category) {
+export function pokeListMainTemplate(category) {
     return `<h1>Pokémon List by ${category}</h1>
             <h2>These are the pokémons that were introduced in the selected ${category}</h2>
-            <ul></ul>`;
+            <ul class="poke-list"></ul>`;
 }
 
-function pokemonListCardTemplate(pokemon) {
+export function pokemonListCardTemplate(pokemon) {
     return `<li class="pokemon-card">
                 <a href="#">
                     <img src="${pokemon}" alt="Image of ${pokemon}"/>
@@ -21,18 +21,27 @@ function pokemonListCardTemplate(pokemon) {
 }
 
 export default class PokemonList {
-    constructor(dataSource, mainContainer, category) {
+    constructor(dataSource, mainContainer) {
         this.dataSource = dataSource;
         this.mainContainer = mainContainer;
-        this.category = category;
+        this.category = getLocalStorage("category");
+        this.pokeList = [];
     }
 
     async init() {
+    // Render PokeList main:
+    renderWithTemplate(pokeListMainTemplate(this.category), this.mainContainer);
+
     // Fill the title with the name of the page:
     document.querySelector(".page-title").textContent = `Pokémon List by ${this.category} | PokéGen`;
-    // Await promise from dataSource:
-    const list = await this.dataSource.getData(this.category);
-    // Render the list
-    renderListWithTemplate(pokemonListCardTemplate, this.listElement, list, "afterbegin");
+
+    // Get number of generation from LocalStorage:
+    let generation = getLocalStorage("generation");
+    console.log(generation);
+
+    // Render list of generations:
+    const genList = this.dataSource.getPokemonsByGeneration(generation);
+    renderListWithTemplate(pokemonListCardTemplate, this.listElement, genList, "afterbegin");
     }
+
 }
