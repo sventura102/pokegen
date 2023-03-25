f88f12ca76d1d2342a3079f01bf1a2747bdc97dd
BelChaparro <a.belenchaparro@gmail.com>
2023-03-25 00:03:48 -0300

    deleted ho-page.js

--- src/js/home-page.js
+++
@@ -1,8 +0,0 @@
-import ExternalServices from "./ExternalServices.mjs";
-import HomePage from "./HomePage.mjs";
-
-const dataSource = new ExternalServices();
-const mainContainer = document.querySelector(".main-content");
-const homePage = new HomePage(dataSource, mainContainer);
-
-homePage.init();
