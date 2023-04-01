1f10554fd1d5be71c531fe142be05235b70edfad
sventura102 <sventura102@gmail.com>
2023-04-01 09:56:04 -0700

    added popup class

--- src/js/SignUp.mjs
+++ src/js/SignUp.mjs
@@ -1,16 +1,24 @@
-import { renderListWithTemplate, renderWithTemplate, setLocalStorage } from "./utils.mjs";
+import { renderWithTemplate } from "./utils.mjs";
 function signupPageTemplate() {
     return `<h1>Signup</h1>
+            <h2>Please fill in this form to receive updates when new Pokémons are released!</h2>
             <form action="" method="post">
                 <input type="text" name="user-name" placeholder=User Name">
                 <input type="email" name="user-email" placeholder="Email">
                 <input type="password" name="user-pass" placeholder="Password">
-                <input type="button" name="signup-btn" value="Signup">
-                </form>`
+                <input type="button" name="signup-btn" value="Signup" onclick="togglePopup()>
+                </form>
+            <div class="popup" id="popUp">
+                <h2>Thank you!</h2>
+                <p>You've created your account. Thanks!</p>
+            </div>`
 }
 
 let signupBtn = document.getElementById("signup-btn");
-let username = document.getElementById("user-name");
+
+function togglePopup() {
+    document.getElementById("popUp").classList.toggle("active");
+}
 
 export default class SignUp {
     constructor(dataSource, mainContainer) {
@@ -24,6 +32,7 @@ async init() {
     //Render SignUp main:
     renderWithTemplate(signupPageTemplate(), this.mainContainer);
 
+    //Click function:
     
 
     }
