c7fac0c60c25eda43965dd2304cbfdfc1892ab96
BelChaparro <a.belenchaparro@gmail.com>
2023-04-01 14:51:53 -0300

    built init, added await for promise

--- src/js/PokeList.mjs
+++ src/js/PokeList.mjs
@@ -1,23 +1,18 @@
 import { renderWithTemplate, renderListWithTemplate, getLocalStorage } from "./utils.mjs";
 
-export function pokeListMainTemplate(category) {
+function pokeListMainTemplate(category) {
     return `<h1>Pokémon List by ${category}</h1>
             <h2>These are the pokémons that were introduced in the selected ${category}</h2>
-            <ul class="poke-list"></ul>`;
+            <ul class="pokemon-list"></ul>`;
 }
 
-export function pokemonListCardTemplate(pokemon) {
+function pokemonListCardTemplate(pokemon) {
     return `<li class="pokemon-card">
-                <a href="#">
-                    <img src="${pokemon}" alt="Image of ${pokemon}"/>
-                    <h3 class="poke-name">${pokemon}</h3>
-                    <ul class="poke-general">
-                        <p class="poke-color">${pokemon}</p>
-                        <p class="poke-type">${pokemon}</p>
-                        <p class="poke-growth">${pokemon}</p>
-                    </ul>
+                <a href="/#/poke-details">
+                    <h3 class="poke-name">${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>
+                    <p>${pokemon.url}</p>
                 </a>
-            </li>`
+            </li>`;
 }
 
 export default class PokemonList {
@@ -26,22 +21,38 @@ export default class PokemonList {
         this.mainContainer = mainContainer;
         this.category = getLocalStorage("category");
         this.pokeList = [];
+        this.listElement = document.querySelector(".pokemon-list");
     }
 
     async init() {
-    // Render PokeList main:
-    renderWithTemplate(pokeListMainTemplate(this.category), this.mainContainer);
-
+    
     // Fill the title with the name of the page:
     document.querySelector(".page-title").textContent = `Pokémon List by ${this.category} | PokéGen`;
 
+    // Render PokeList main:
+    renderWithTemplate(pokeListMainTemplate(this.category), this.mainContainer);
+
     // Get number of generation from LocalStorage:
     let generation = getLocalStorage("generation");
-    console.log(generation);
+    
+    // Await promise from dataSource:
+    this.pokeList = await this.dataSource.getPokemonsByGeneration(generation);
 
     // Render list of generations:
-    const genList = this.dataSource.getPokemonsByGeneration(generation);
-    renderListWithTemplate(pokemonListCardTemplate, this.listElement, genList, "afterbegin");
+    renderListWithTemplate(pokemonListCardTemplate, this.listElement, this.pokeList);
     }
 
 }
+
+/* <img src="${pokemon}" alt="Image of ${pokemon}"/>
+<p class="poke-color">${pokemon}</p>
+<p class="poke-type">${pokemon}</p>
+<p class="poke-growth">${pokemon}</p> */
+
+/*
+addProductToCart() {
+    let cart = getLocalStorage("so-cart") || [];
+    cart.push(this.product);
+    setLocalStorage("so-cart", cart);
+}
+      */
