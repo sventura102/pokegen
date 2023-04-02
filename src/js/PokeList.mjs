b09eb6c0e33692f167e9940f854c9f2f22a6f159
BelChaparro <a.belenchaparro@gmail.com>
2023-04-01 23:53:34 -0300

    added list of pokemons and sorted names alphabetically

--- src/js/PokeList.mjs
+++ src/js/PokeList.mjs
@@ -1,17 +1,17 @@
-import { renderWithTemplate, renderListWithTemplate, getLocalStorage } from "./utils.mjs";
+import { renderWithTemplate, renderListWithTemplate, getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function pokeListMainTemplate(category) {
     return `<h1>Pokémon List by ${category}</h1>
             <h2>These are the pokémons that were introduced in the selected ${category}</h2>
-            <ul class="pokemon-list"></ul>`;
+            <ul id="pokemon-list"></ul>`;
 }
 
 function pokemonListCardTemplate(pokemon) {
     return `<li class="pokemon-card">
                 <a href="/#/poke-details">
                     <h3 class="poke-name">${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>
-                    <p>${pokemon.url}</p>
                 </a>
+                <button type="submit" id="${pokemon.id} value="${pokemon.name}">Vote for Me!</button>
             </li>`;
 }
 
@@ -21,7 +21,8 @@ export default class PokemonList {
         this.mainContainer = mainContainer;
         this.category = getLocalStorage("category");
         this.pokeList = [];
-        this.listElement = document.querySelector(".pokemon-list");
+        this.pokeInfo = [];
+        this.sortBy = "name"; // initialize the default sorting order
     }
 
     async init() {
@@ -38,13 +39,65 @@ export default class PokemonList {
     // Await promise from dataSource:
     this.pokeList = await this.dataSource.getPokemonsByGeneration(generation);
 
+    // Sort the list based on the default sorting order(name):
+    this.sortPokeList(this.pokeList);
+
+    // Set the pokemon information in the pokeList:
+    this.pokeList.forEach(pokemon => {
+        const url = pokemon.url.slice(0, -1);
+        const pokeId = url.substring(url.lastIndexOf("/") + 1);
+        pokemon.id = pokeId;
+
+        // //Get pokemon info from API:
+        // this.getPokeInfo(pokeId);s
+
+    });
+    console.log(this.pokeList);
+
+    // Get the options parent elements:
+    const pokemonListElement = document.querySelector("#pokemon-list");
+
     // Render list of generations:
-    renderListWithTemplate(pokemonListCardTemplate, this.listElement, this.pokeList);
+    renderListWithTemplate(pokemonListCardTemplate, pokemonListElement, this.pokeList);
+
+    // Listen for click on the vote button:
+   
+
+    // let buttons = document.getElementsByTagName("button");
+    // let buttonsCount = buttons.length;
+    // for (let i = 0; i <= buttonsCount; i += 1) {
+    //     buttons[i].onclick = () => {
+    //         if (getLocalStorage("votes").includes(this.value)) {
+    //             let pokeId = buttons[i].getAttribute("id");
+    //             let pokeName = buttons[i].getAttribute("value");
+
+    //             console.log("it's included!")
+    //         } else {
+    //             console.log("it's not included :(")
+    //         }
+                
+    //     };
+    // }
+
+
+    }
+
+    // Sorts the pokeList alphabetically:
+    sortPokeList(pokeList) {
+        if (this.sortBy === "name") {
+            pokeList.sort((a, b) => a.name.localeCompare(b.name));
+        }
+    }
+
+    // Get pokémon information:
+    async getPokeInfo(pokeId) {
+        this.pokeInfo.push(await this.dataSource.findPokemonById(pokeId));
     }
 
 }
+/* const pokeInfo = await this.dataSource.findPokemonById(pokeId); */
 
-/* <img src="${pokemon}" alt="Image of ${pokemon}"/>
+/* <img src="${pokemon.sprites}" alt="Image of ${pokemon.name}">"/>
 <p class="poke-color">${pokemon}</p>
 <p class="poke-type">${pokemon}</p>
 <p class="poke-growth">${pokemon}</p> */
