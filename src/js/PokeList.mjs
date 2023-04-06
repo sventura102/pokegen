d8db5be212f07358bc29cae7c942cf7533919da3
BelChaparro <a.belenchaparro@gmail.com>
2023-04-05 21:47:12 -0300

    added types listing

--- src/js/PokeList.mjs
+++ src/js/PokeList.mjs
@@ -1,25 +1,42 @@
 import { renderWithTemplate, renderListWithTemplate, getLocalStorage, setLocalStorage } from "./utils.mjs";
 
-function pokeListMainTemplate(category) {
-    return `<h1>Pokémon List by ${category}</h1>
-            <h2>These are the pokémons that were introduced in the selected ${category}</h2>
+function pokeListMainTemplate(headerInfo) {
+    let h2 = "";
+    if (headerInfo[0] == "generation") {
+        h2 = `<h2>These are the pokémons that were introduced in ${headerInfo[0]} ${headerInfo[1]}</h2>`;
+    } else {
+        h2 = `<h2>These are the pokémons that have ${headerInfo[0]} "${headerInfo[1]}"</h2>`;
+    }
+    return `<h1>Pokémon List by ${headerInfo[0]}</h1>
+            ${h2}
             <ul id="pokemon-list"></ul>`;
 }
 
 function pokemonListCardTemplate(pokemon) {
+    let id = 0;
+    let name = "";
+    if (getLocalStorage("category") == "generation") {
+        id = pokemon.id;
+        name = pokemon.name;
+    } else if (getLocalStorage("category") == "types") {
+        id = pokemon.pokemon.id;
+        name = pokemon.pokemon.name;
+    }
     return `<li class="pokemon-card">
-                <a href="/#/poke-details" id="${pokemon.id}">
-                    <h3 class="poke-name">${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>
+                <a href="/#/poke-details" id="${id}">
+                    <h3 class="poke-name">${name}</h3>
                 </a>
-                <button type="submit" id="${pokemon.name}">Vote for Me!</button>
+                <button type="submit" id="${name}">Vote for Me!</button>
             </li>`;
 }
 
 export default class PokemonList {
     constructor(dataSource, mainContainer) {
         this.dataSource = dataSource;
-        this.mainContainer = mainContainer;
+        this.mainContainer = mainContainer;        
         this.category = getLocalStorage("category");
+        this.selection = getLocalStorage(this.category);
+        this.headerInfo = [this.category, this.selection];
         this.pokeList = [];
         this.pokeInfo = [];
         this.sortBy = "name"; // initialize the default sorting order
@@ -31,32 +48,57 @@ export default class PokemonList {
     document.querySelector(".page-title").textContent = `Pokémon List by ${this.category} | PokéGen`;
 
     // Render PokeList main:
-    renderWithTemplate(pokeListMainTemplate(this.category), this.mainContainer);
-
-    // Get number of generation from LocalStorage:
-    let generation = getLocalStorage("generation");
-    
-    // Await promise from dataSource:
-    this.pokeList = await this.dataSource.getPokemonsByGeneration(generation);
-
-    // Sort the list based on the default sorting order(name):
-    this.sortPokeList(this.pokeList);
-
-    // Set the pokemon information in the pokeList:
-    this.pokeList.forEach(pokemon => {
-        const url = pokemon.url.slice(0, -1);
-        const pokeId = url.substring(url.lastIndexOf("/") + 1);
-        pokemon.id = pokeId;
-
-        // //Get pokemon info from API:
-        // this.getPokeInfo(pokeId);s
-
-    });
+    renderWithTemplate(pokeListMainTemplate(this.headerInfo), this.mainContainer);
 
     // Get the options parent elements:
     const pokemonListElement = document.querySelector("#pokemon-list");
 
-    // Render list of generations:
+    if (this.category == "generation") {
+
+        // Get number of generation from LocalStorage:
+        let generation = getLocalStorage("generation");
+        
+        // Await promise from dataSource:
+        this.pokeList = await this.dataSource.getPokemonsByGeneration(generation);
+
+        // Sort the list based on the default sorting order(name):
+        this.sortPokeList(this.pokeList);
+
+        // Set the pokemon information in the pokeList:
+        this.pokeList.forEach(pokemon => {
+            const url = pokemon.url.slice(0, -1);
+            const pokeId = url.substring(url.lastIndexOf("/") + 1);
+            pokemon.id = pokeId;
+        });
+
+    } else {
+
+        // Get types from localStorage (array with types):
+        let types = getLocalStorage("types");
+        let pokeByTypes = [];
+        // Await promise from dataSource:
+        for (let type in types) {
+            console.log(types[type])
+            pokeByTypes.push( await this.dataSource.getPokemonsByType(types[type]));
+            //Object.assign(this.pokeList, {[types[type]] : await this.dataSource.getPokemonsByType(types[type])});
+        }
+        this.pokeList = pokeByTypes;
+
+        // Set the pokemon information in the pokeList:
+        this.pokeList.forEach(type => {
+            type.forEach( pokemon => {
+                const url = pokemon.pokemon.url.slice(0, -1);
+                const pokeId = url.substring(url.lastIndexOf("/") + 1);
+                pokemon.pokemon.id = pokeId;
+
+                this.pokeList.push(pokemon);
+            })
+        });
+        this.pokeList.splice(0, 3);
+
+    }
+
+    // Render list of pokémons:
     renderListWithTemplate(pokemonListCardTemplate, pokemonListElement, this.pokeList);
 
     // Listen for click on button:
@@ -105,6 +147,11 @@ export default class PokemonList {
     // Get pokémon information:
     async getPokeInfo(pokeId) {
         this.pokeInfo = await this.dataSource.findPokemonById(pokeId);
+    }
+
+    //Get pokémons based on types:
+    async getPokemonsByType(type) {
+        return await this.dataSource.getPokemonsByType(type);
     }
 
 }
